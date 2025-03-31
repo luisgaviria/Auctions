9e17cda16b4886725ef246c4bf0ffdc162e9d394
kurs0n <patryk744@op.pl>
2025-03-31 16:35:14 +0200

    feat: dockerfile and working deployment

--- backend/go.mod
+++ backend/go.mod
@@ -1,6 +1,8 @@
 module backendAuction
 
-go 1.23.1
+go 1.21
+
+toolchain go1.23.4
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
--- backend/main.go
+++ backend/main.go
@@ -61,7 +61,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler: router,
-		Addr:    "127.0.0.1:" + port,
+		Addr:    ":" + port,
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
